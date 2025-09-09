b24d89665d3f39de480bdf9bcfdba84afe984025
dmsemy <dmytrosemysiuk@gmail.com>
2025-09-09 19:04:51 +0200

    chore(phpcs): Integrate PHP_CodeSniffer with Symfony ruleset
            - Added phpcs.xml.dist with Symfony coding standard
            - Added composer lint:phpcs and lint:phpcs:fix scripts

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,6 +7,13 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
+/**
+ * The application kernel.
+ *
+ * This class configures the Symfony application, including bundles,
+ * routes, and container services. It extends the base kernel and
+ * uses the MicroKernelTrait for simplified setup.
+ */
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
