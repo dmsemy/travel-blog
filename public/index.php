bee247d37b10f7567b7d7c0ab07c3fad8ad68b16
dmsemy <dmytrosemysiuk@gmail.com>
2025-09-18 11:19:22 +0200

    chore(grumphp): Add pre-push git hook, add composer script
    - Fixed phpstan errors

--- public/index.php
+++ public/index.php
@@ -5,5 +5,11 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    if (!isset($context['APP_ENV']) || !is_string($context['APP_ENV'])) {
+        throw new \RuntimeException('APP_ENV must be a string.');
+    }
+    if (!isset($context['APP_DEBUG']) || !is_bool($context['APP_DEBUG'])) {
+        throw new \RuntimeException('APP_DEBUG must be a boolean.');
+    }
+    return new Kernel($context['APP_ENV'], $context['APP_DEBUG']);
 };
